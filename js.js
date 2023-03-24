36fc5926dcb7315ee943455a6398683f6fb3257c
Ljut <ljutikazlatan@gmail.com>
2023-03-24 23:17:58 +0100

    lyrics

--- js.js
+++ js.js
@@ -1,4 +1,8 @@
 function songToPlay(layer=0) {
+        let x = document.getElementById("opis");
+        let y = document.getElementById("naslov");
+        let z = document.getElementById("tekst");
+
         let songLyrics="sounds/audio_tiny44.wav";
         let songNoLyrics="sounds/catch-action.wav";
         console.log(layer);
@@ -7,13 +11,27 @@ function songToPlay(layer=0) {
                 path+="../";
         }
         if(document.getElementById("lyrics").checked==true) {
+                
                 console.log("Sviraj s textom");
                 path+=songLyrics;
                 document.getElementById("pjesma").src=path;
+
+                x.style.display = "block";
+                y.style.display = "block";
+                z.style.display = "block";
         }
         else {
                 console.log("Ne Sviraj s textom")
                 path+=songNoLyrics;
                 document.getElementById("pjesma").src=path;
+
+                x.style.display = "none";
+                y.style.display = "none";
+                z.style.display = "none";
         }
+        
+          
+          
+          
+
     }
